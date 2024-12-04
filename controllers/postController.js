cebf9f2e33c217336b04026ae96df179bda1f280
cdevelopment010 <cdevelopment010@gmail.com>
2024-12-04 16:05:03 +0000

    Feat: basic post controller

---
+++ controllers/postController.js
@@ -0,0 +1,39 @@
+const db = require("../prisma/queries"); 
+
+const createPost = async(req, res, next) => {
+
+}
+
+const publishPost = async (req, res, next) => {
+
+}
+
+const unpublishPost = async(req, res, next) => {
+
+}
+
+const getPostById = async(req, res, next) => {
+
+}
+
+const updatePost = async(req, res, next) => {
+
+}
+
+const deletePost = async(req, res, next) => {
+
+}
+
+
+
+
+
+module.exports = {
+    createPost,
+    publishPost, 
+    unpublishPost,
+    getPostById,
+    updatePost,
+    deletePost,
+
+}
