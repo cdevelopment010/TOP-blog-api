4e42fe2f270a1a0b24f3eae54cb299cf2cc5c948
Craig <cdevelopment010@gmail.com>
2024-12-07 16:21:17 +0000

    Feat: basic JWT added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const jwt = require("jsonwebtoken");
 const app = express(); 
 const PORT = process.env.PORT || 3000;
+
 
 const indexRouter = require("./routes/index");
 const postRouter = require("./routes/post");
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,47 @@
 const { Router } = require("express"); 
+const jwt = require("jsonwebtoken");
+const userMiddleware = require("../middleware/user"); 
 const router = Router(); 
+const SECRET_KEY = process.env.SECRET_KEY || 'secret-key'
 
 
 router.get("/", (req, res) => {
     res.send("HOME!");
 })
+router.post("/signup", async (req, res) => {
+    //create user with bycrypt needed
+})
 
-router.get("/Tags", (req, res) => {
-    res.send("TAGS")
+router.get("/login", (req, res) => {
+    //authenticate user needed
+
+    const user = { 
+        id: 1, 
+        username: 'admin', 
+        email: 'admin@example.com'
+    }
+    
+    jwt.sign({user: user}, SECRET_KEY, {expiresIn: '1h'},  (err, token) => {
+        res.json({
+            token: token
+        })
+    })
+
+    //front end should save token to local storage
+})
+
+router.get("/Tags", userMiddleware.verifyToken, (req, res) => {
+    jwt.verify(req.token, SECRET_KEY, (err, authData) => {
+        if(err) {
+            res.sendStatus(403); 
+        } else { 
+            console.log();
+            res.json({
+                message: "TAGS",
+                auth: authData
+            })
+        }
+    });
 })
 
 router.post("/Tags", (req, res) => {
