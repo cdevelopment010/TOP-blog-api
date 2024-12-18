0bc45c10ad42543bc118e237ce4186ab2d497010
cdevelopment010 <cdevelopment010@gmail.com>
2024-12-18 19:09:35 +0000

    Fix: middleware for posts

--- routes/post.js
+++ routes/post.js
@@ -1,17 +1,18 @@
 const {Router} = require("express"); 
 const router = Router(); 
 const controller = require("../controllers/postController");
+const middleware = require("../middleware/user");
 
-router.get("/", controller.getAllPosts);
-router.post("/", controller.createPost);
-router.get("/:postId", controller.getPostById);
-router.delete("/:postId", controller.deletePost);
-router.put("/:postId", controller.updatePost);
+router.get("/", middleware.verifyToken, controller.getAllPosts);
+router.post("/", middleware.verifyToken, controller.createPost);
+router.get("/:postId", middleware.verifyToken, controller.getPostById);
+router.delete("/:postId", middleware.verifyToken, controller.deletePost);
+router.put("/:postId", middleware.verifyToken, controller.updatePost);
 
-router.get("/author/:authorId", controller.getPostByAuthor);
-router.get("/tag/:tagId", controller.getPostByTag);
+router.get("/author/:authorId", middleware.verifyToken, controller.getPostByAuthor);
+router.get("/tag/:tagId", middleware.verifyToken, controller.getPostByTag);
 
-router.get("/:postId/Comments/:commentId", controller.getCommentById);
+router.get("/:postId/Comments/:commentId",controller.getCommentById);
 router.post("/:postId/Comments", controller.createComment);
 router.delete("/:postId/Comments/:commentId", controller.deleteCommentById);
 router.put("/:postId/Comments/:commentId", controller.updateCommentById);
