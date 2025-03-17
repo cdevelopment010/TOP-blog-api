6b876e3d2a88956ff083b6d5d48cc54d17605f4c
cdevelopment010 <cdevelopment010@gmail.com>
2025-03-17 09:52:29 +0000

    Fix: Filter out 0 tag counts

--- prisma/queries.js
+++ prisma/queries.js
@@ -464,6 +464,9 @@ exports.getPostCountByTag = async () => {
             },
         });
 
+        // Filter out tags where the count is 0
+        const filteredTags = tags.filter(tag => tag._count.PostTag > 0);
+
         // Sort by the number of published posts associated with each tag
         const sortedTags = tags.sort((a, b) => b._count.PostTag - a._count.PostTag);
 
