bddbd10a82263d43bf169b17e8bd11157933504c
cdevelopment010 <cdevelopment010@gmail.com>
2025-04-08 08:30:33 +0100

    Feat: Sitemap route

---
+++ controllers/index.js
@@ -0,0 +1,41 @@
+const db = require("../prisma/queries"); 
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+
+const createSitemap = async (req, res) => {
+    try { 
+        const posts = await db.findAllPublishedPosts(1, 1000); 
+
+        const postLinks = posts.map(post => ({
+            url: `/post/${post.slug}`,
+            changefreq: 'weekly', 
+            priority: 0.8, 
+            lastmod: post.updated_ad || post.created_at,
+        }))
+
+        //static urls;
+        const staticLinks = [
+            {url: '/', changefreq: 'weekly', priority: 1.0},
+            {url: '/posts', changefreq: 'weekly', priority: 0.9},
+            {url: '/tags', changefreq: 'weekly', priority: 0.8},
+            {url: '/about', changefreq: 'weekly', priority: 0.5}
+        ]
+
+        const allLinks = [...staticLinks, ...postLinks]; 
+
+        const stream = new SitemapStream({hostname: 'https://coffeeshopcoding.dev'})
+
+        res.header('content-type', 'application/xml'); 
+        const xml = await streamToPromise(Readable.from(allLinks).pipe(stream)).then(data => data.toString()); 
+        res.send(xml); 
+        return; 
+    } catch(err) { 
+        console.error("Sitemap generation error:", err); 
+        res.status(500).end(); 
+        return;
+    }
+} 
+
+module.exports = { 
+    createSitemap
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require("express"); 
+const controller = require("../controllers/index");
+
 const router = Router(); 
+
 
 router.get("/", (req, res) => {
     res.send("HOME!");
 })
+
+router.get("/sitemap.xml",controller.createSitemap); 
 
 module.exports = router;
